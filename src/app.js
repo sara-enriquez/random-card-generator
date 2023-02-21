e0030a7f0c4307a860b8a9a77c213916f6800bb5
Sara Enríquez <sara_enriquezdesalamanca@hotmail.com>
2023-02-21 22:00:46 +0000

    created card

--- src/app.js
+++ src/app.js
@@ -2,10 +2,46 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const topSuits = document.querySelector("#topSuits");
+  const numContainer = document.querySelector("#numContainer");
+  const bottomSuits = document.querySelector("#bottomSuits");
+  const newCardBtn = document.querySelector("#newCardBtn");
 };
+
+newCardBtn.addEventListener("click", () => {
+  let numeros = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let palos = ["♦", "♥", "♠", "♣"];
+  const random = () => {
+    let paloRandom = Math.floor(Math.random() * palos.length);
+    let numeroRandom = Math.floor(Math.random() * numeros.length);
+    let randomPalo = palos[paloRandom];
+    let randomNumero = numeros[numeroRandom];
+    topSuits.innerHTML = randomPalo;
+    numContainer.innerHTML = randomNumero;
+    bottomSuits.innerHTML = randomPalo;
+
+    if (randomPalo == palos[0] || randomPalo == palos[1]) {
+      topSuits.style.color = "red";
+      bottomSuits.style.color = "red";
+    } else {
+      topSuits.style.color = "black";
+      bottomSuits.style.color = "black";
+    }
+  };
+  random();
+});
