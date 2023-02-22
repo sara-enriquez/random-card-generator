a35d73b2768544595ca3d921baef2f2991fb6634
Sara Enríquez <sara_enriquezdesalamanca@hotmail.com>
2023-02-22 16:52:00 +0000

    remover margin of numContainer

--- src/app.js
+++ src/app.js
@@ -42,6 +42,10 @@ window.onload = function() {
       topSuits.style.fontSize = "15px";
       bottomSuits.style.fontSize = "15px";
       numContainer.style.fontSize = "25px";
+    } else {
+      topSuits.style.fontSize = "50px";
+      bottomSuits.style.fontSize = "50px";
+      numContainer.style.fontSize = "80px";
     }
   };
 };
